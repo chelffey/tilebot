81f22b845db3bf03b340049c23ac8daebb9d2999
chelffey-pc <chaffeychelsea@gmail.com>
2021-05-12 14:20:36 +1000

    working on manipulating and creating transparent pngs

--- src/tiler.py
+++ src/tiler.py
@@ -9,7 +9,7 @@ from PIL import Image
 size = 136, 136
 
 '''
-crops to maximum size square, like 'cover'. 
+Crops given 'Image' object to the largest square possible. 
 '''
 def crop_square(im):
     # crops to square, based on smallest length.
@@ -26,11 +26,13 @@ def crop_square(im):
     # print(f"side length: {side_length}")
 
 '''
-Crops image to a square. 
-Then returns as an np array thumbnail.
+Opens the given image file from local directory. 
+Returns as an np array of thumbnail size. 
 '''
 def gen_thumbnail(filename):
     with Image.open(filename) as im:
+        im = im.convert("RGBA")
+        print(im.size)
         im = crop_square(im)
         im.thumbnail(size)
         a = np.asarray(im)
@@ -40,15 +42,33 @@ if __name__ == "__main__":
     print("hello world!! im tilebot")
     
     # open np thumbnails
+    d = gen_thumbnail("./pic/bamboo.jpg")
     a = gen_thumbnail("./pic/coconut.png")
     b = gen_thumbnail("./pic/fish.png")
-
+    c = gen_thumbnail("./pic/shiro.jpg")
+    #d = gen_thumbnail("./pic/calico-cat.png")
 
     lx, ly, lz = a.shape
-    print(f"dim x={lx}, y={ly}, z={lz}")
+    bx, by, bz = b.shape
+    cx, cy, cz = c.shape
+    print(f"A: dim x={lx}, y={ly}, z={lz}")
+    print(f"B: dim x={bx}, y={by}, z={bz}")
+    print(f"C: dim x={cx}, y={cy}, z={cz}")
+    dx, dy, dz = d.shape
+    print(f"D: dim x={dx}, y={dy}, z={dz}")
+    # clearly, sometimes the dimensions are one pixel off. 
 
-    arr = np.concatenate((a, b))
-    # arr = np.hstack((a, b), axis=1)
+    # TESTTTTTTTT
+    row = np.full((100, 20, 4), [255, 255, 255, 0], np.uint8) # must be unsigned integer to use.
+    test = np.full((100, 20, 4), [255, 100, 0, 255], np.uint8)
+    arr = np.concatenate((row, test, row, test))
+    im = Image.fromarray(arr)
+    im.save("./pic/TEST.png", "PNG")
+
+    arr = np.concatenate((a, b, c, d))
+    arr2 = np.hstack((a, c, d))
 
     im = Image.fromarray(arr)
     im.save("./pic/merge-thumbnail-2.png", "PNG")
+    im = Image.fromarray(arr2)
+    im.save("./pic/merge-thumbnail-3.png", "PNG")
